eca040cfa79b53784e82bd5449e3f82d02356814
Christian Benedict Marie Montilla Espinosa <christianbenedictmarieespinosa@gmail.com>
2026-08-12 10:20:59 +0800

    I will finish this at home

---
+++ Lab2/page1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About me</title>
+</head>
+<body>
+    <?php
+        $titlePage1 = "About me";
+        $name = "Christian Benedict Marie Espinosa";
+        $course = "Bachelor of Science in Computer Science - Major in Game Development";
+
+        echo $titlePage1;
+        echo $name;
+        echo $course;
+        ?>
+</body>
+</html> 
